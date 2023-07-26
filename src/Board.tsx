157741927d01d279126d2416f97d82b26243dc86
Enrique Gan <enriqueganpit@gmail.com>
2023-07-26 00:12:03 -0700

    working on board editing

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,29 +1,52 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import Cell from './Cell';
 // import {DigitContext} from './Contexts';
 
 function Board() {
-  const [digit,setDigit] = useState(1);
-  
+
+  // check function
   const check = () => {
-    console.log('board check'.concat(digit.toString()));
+    console.log('board check');
   };
 
+  // cells
+  const [selectedCell,setSelectedCell] = useState(-1);
   let cells = Array(9*9);
   // key every cell a0 to i8
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
       const key = String.fromCharCode(i+'a'.charCodeAt(0)).concat(j.toString());
-      cells[(i*9)+j] = <Cell key={key} check={check} lol={digit}/>
+      const cellId = i*9+j;
+      cells[cellId] = <Cell 
+        key={key} 
+        digit={0}
+        clickFunc={()=>{setSelectedCell((i*9)+j)}}/>
     }
   }
+
+  // not working, being overwritten on next render
+  const updateCell = (id : number, digit : number) => {
+    const cell = cells[id];
+    cells[id] = <Cell
+      key={cell.key}
+      digit={digit}
+      clickFunc={cell.props.clickFunc}/>
+    console.log(cells[id]);
+  }
+
+  // digit buttons
+  let digitButtons = [...Array(9)].map((_,i) => 
+    <button key={`d${i}`} onClick={() => {
+      updateCell(selectedCell, i+1);
+    }}>{i+1}
+      </button>
+    );
 
   return (
       <div className="board">
         here's the board.
-        <button onClick={()=>{setDigit(digit+1)}}> lmao</button>
-        {digit}
+        <p>selected cell:{selectedCell}</p>
+        {digitButtons}
         {cells}
       </div>
   );
--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,27 +1,19 @@
-import React from 'react';
-import {useState, useContext} from 'react';
+import {useState} from 'react';
 // import {DigitContext} from './Contexts';
 
 interface cell {
   key: string,
-  check: ()=>void,
-  lol: number,
+  digit: number,
+  clickFunc: ()=>void,
 }
 
 function Cell(props : cell) {
   let [fun,setFun] = useState(0);
-  // let context = useContext(DigitContext);
-
-  const clickFunc = () => {
-    setFun(fun+1);
-    props.check();
-    console.log(props.lol);
-  };
 
   return (
       <div className="cell">
-      here's a cell {fun}
-      <button onClick={clickFunc}> click me </button>
+      here's a cell {props.digit}
+      <button onClick={props.clickFunc}> click me </button>
       </div>
   );
 }
